d6e8470c7bbbdb0047d5f0791ef748f52f4b6a4f
FedorovaEP <149509604+FedorovaEP@users.noreply.github.com>
2023-11-26 22:23:27 +0300

    Add files via upload

---
+++ Лабораторная работа №3/Task_1.py
@@ -0,0 +1,9 @@
+import json
+def task():
+    with open('input.json') as f:
+        data = json.load(f)
+    summa = sum([item["score"] * item["weight"] for item in data])
+    return round(summa, 3)
+
+
+print(task())
